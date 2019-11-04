694726ff85123ef7f0f0a6725207a1e3b9115739
root <root@localhost.localdomain>
2019-11-05 00:42:20 +0700

    Test

--- arescode.py
+++ arescode.py
@@ -3,15 +3,18 @@
 import sys
 sys.path.append('/usr/bin/library/')
 
+from termcolor import colored
+from threading import Thread
+from http_helpers import *
+from string_helpers import *
 import pyfiglet
 import requests
 import argparse
-from termcolor import colored
 import os
-from http_helpers import *
-from string_helpers import *
+import datetime
 import time
 
+t = 1
 
 info = """
             Author: Johan Du
@@ -20,7 +23,67 @@ info = """
             Description: Create domain directory structure based on response code
 """
 
+class Request_performer(Thread):
+
+    def __init__(self, url, stt, results):
+
+        Thread.__init__(self)
+        self.url = url
+        self.stt = stt
+        self.results = results
+
+    def run(self):
+
+        response_code_structure(self.url, self.stt, self.results)
+
+def response_code_structure(url, stt, results):
+
+    global t
+
+    try:
+        string = ''
+        url = url.strip()
+        if isHttps(url):
+            domain = delete_https(url)
+        else:
+            domain = delete_http(url)
+        string += str(stt) + '\n'
+        string +=  colored('Original: ', 'yellow') + url + '\n'
+        res = requests.get(url, headers=gen_headers)
+        code = int(res.status_code)
+        if not len(res.history):
+            if code >= 100 and code < 200:
+                os.system('mkdir -p ' + results + '/1xx/' + str(code) + "/" + domain)
+            if code >= 200 and code < 300:
+                os.system('mkdir -p ' + results + '/2xx/' + str(code) + "/" + domain)
+            if code >= 400 and code < 500:
+                os.system('mkdir -p ' + results + '/4xx/' + str(code) + "/" + domain)
+            if code >= 500:
+                os.system('mkdir -p ' + results + '/5xx/' + str(code) + "/" + domain)
+        else:
+            os.system('mkdir ' + results + '/3xx/' + domain)
+            fw = open(results + '/3xx/' + domain  + '/' + domain + '_redirect', 'w')
+            for history in res.history:
+                fw.write(history.request.url + "\t\t\t" + str(history.status_code) + "\n")
+                fw.write('-'*200 + "\n")
+            fw.write(res.request.url + "\t\t\t" + str(res.status_code) + "\n")
+            fw.close()
+            string +=  colored('Redirect: ', 'yellow') + res.request.url
+        string +=  colored('Response code: ', 'yellow') + colored(res.status_code, 'blue') + '\n'
+        string +=  colored('History: ', 'yellow') + str(res.history) + '\n'
+    except Exception as error: 
+        string +=  colored('Error: ', 'yellow') + colored(error, 'red') + '\n'
+        os.system('mkdir ' + results + '/error/' + domain)
+        fw = open(results + '/error/' + domain + '/' + domain + '_error', 'w')
+        fw.write(str(error))
+        fw.close()
+    string +=  colored('-'*80, 'white') + '\n'
+    print string
+    t -= 1
+
 def main():
+
+    global t
 
     # Construct the argument parser
     ap = argparse.ArgumentParser()
@@ -28,20 +91,32 @@ def main():
     # Add the arguments to the parser
     ap.add_argument("-f", "--file", required=True, help="Enter the filename contain URLs")
     ap.add_argument("-o", "--output", required=True, help="Return the resulting directory name")
+    ap.add_argument("-t", "--thread", help="Number of threads")
+
     args = vars(ap.parse_args())
     files = args['file']
     results = args['output']
+    totalthread = args['thread']
 
+    if totalthread == None:
+        totalthread = 50
+    else:
+        totalthread = int(totalthread)
+
+    countthread = 0
+    threads = []
     os.system('clear')
 
     ascii_banner = pyfiglet.figlet_format("Arescode")
     print ascii_banner
     print colored(info, 'yellow')
-    print colored('\nWait a few seconds\n\n\n', 'blue')
+    print colored('\nWait a few seconds\n', 'blue')
     time.sleep(3)    
 
+    start = time.time()
+
     f = open(files, 'r')
-    domains = f.readlines()
+    urls = f.readlines()
 
     if os.path.exists(results):
         os.system('rm -r ' + results)
@@ -53,53 +128,24 @@ def main():
     os.system('mkdir ' + results + '/5xx')
     os.system('mkdir ' + results + '/error')
 
-    count = 1
+    count = 0
 
-    for domain in domains:
-        try:
-            domain = domain.strip()
-            url = domain
-            if isHttps(url):
-                domain = delete_https(url)
-            else:
-                domain = delete_http(url)
-             
-            print str(count)
-            print colored('Original: ', 'yellow') + url
+    for url in urls:
+        count += 1
+        while t > int(totalthread):
+            continue
+        t += 1
+        thread = Request_performer(url, count, results)
+        thread.start()
+        threads.append(thread) 
+     
+    for thread in threads:
+        thread.join()
+
         
-            count += 1
 
-            res = requests.get(url, headers=gen_headers)
-
-            code = int(res.status_code)
-            if not len(res.history):
-                if code >= 100 and code < 200:
-                    os.system('mkdir -p ' + results + '/1xx/' + str(code) + "/" + domain)
-                if code >= 200 and code < 300:
-                    os.system('mkdir -p ' + results + '/2xx/' + str(code) + "/" + domain)
-                if code >= 400 and code < 500:
-                    os.system('mkdir -p ' + results + '/4xx/' + str(code) + "/" + domain)
-                if code >= 500:
-                    os.system('mkdir -p ' + results + '/5xx/' + str(code) + "/" + domain)
-            else:
-                os.system('mkdir ' + results + '/3xx/' + domain)
-                fw = open(results + '/3xx/' + domain  + '/' + domain + '_redirect', 'w')
-                for history in res.history:
-                    fw.write(history.request.url + "\t\t\t" + str(history.status_code) + "\n")
-                    fw.write('-'*200 + "\n")
-                fw.write(res.request.url + "\t\t\t" + str(res.status_code) + "\n")
-                fw.close()
-           
-            print colored('Redirect: ', 'yellow') + res.request.url
-            print colored(res.status_code, 'blue')
-            print res.history
-        except Exception as error:
-            print colored(error, 'red')
-            os.system('mkdir ' + results + '/error/' + domain)
-            fw = open(results + '/error/' + domain + '/' + domain + '_error', 'w')
-            fw.write(str(error))
-            fw.close()
-        print colored('-'*80, 'white')
+    end = time.time()
+    print colored('Time hangling proxies is hh:mm:ss: ', 'yellow') + str(datetime.timedelta(seconds=int(end - start)))
          
 if __name__ == '__main__':
     main()
